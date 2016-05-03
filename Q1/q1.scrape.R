1fddb09ffd90c728d3d8b5958a786aaa0c3b1e78
yahwes <janet.wesner@outlook.com>
2016-05-03 09:50:35 -0500

    add q1 files

---
+++ Q1/q1.scrape.R
@@ -0,0 +1,24 @@
+require(dplyr)
+require(rvest)
+
+options(stringsAsFactors = FALSE)
+
+url_base <- "http://securities.stanford.edu/list-mode.html?page="
+tbl.clactions <- data.frame(
+  "Filing.Name" = character(0),
+  "Filing.Date" = character(0),
+  "District.Court" = character(0),
+  "Exchange" = character(0),
+  "Ticker" = character(0) )
+
+for (i in 1:ceiling(4149/20)) {  # total filings: 4149, listed 20 per page
+  url <- paste0(url_base, i)
+  tbl.page <- url %>%
+    read_html() %>%
+    html_nodes(xpath='//*[@id="records"]/table') %>%
+    html_table()
+  names(tbl.page[[1]]) <- names(tbl.clactions)
+  tbl.clactions <- bind_rows(tbl.clactions, tbl.page[[1]])
+}
+
+write.csv(tbl.clactions, file="class_actions.csv", row.names=FALSE)
